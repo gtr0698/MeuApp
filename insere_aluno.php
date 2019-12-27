2f643a9ed160a56edb1f6eb2a6edf2792113cc68
guilherme <guilherme_taborda@live.com>
2019-12-27 15:45:10 -0300

    Formulario de inserção

--- insere_aluno.php
+++ insere_aluno.php
@@ -1,8 +1,17 @@
 <?php 
 
-$nome = $_POST["nome"];
-$sobrenome = $_POST["sobrenome"];
+$dados = $_POST;
 
-echo $nome . " <br> " . $sobrenome;
+$conn = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+
+$sql = 'insert into tb_aluno (nome, registro, dtnascimento, ingresso) values (' 
+	."'" . $dados['nome'] ."'" . ','
+	."'" . $dados['registro'] ."'" . ','
+	."'" . $dados['dtnascimento'] ."'" . ','
+	."'" . $dados['ingresso'] ."'" . ')';
+
+$stmt = $conn->prepare($sql);
+
+$stmt->execute();
 
 ?>
